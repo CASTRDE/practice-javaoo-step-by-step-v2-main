bec560f08d4a276cc2183f0fed166e85d27222c8
Desiree Castro <desiree.anne.castro@oocl.com>
2022-08-31 17:12:33 +0800

    test: Completed test 1

--- src/main/java/ooss/Person.java
+++ src/main/java/ooss/Person.java
@@ -17,4 +17,19 @@ public class Person {
     public String introduce() {
         return MessageFormat.format("My name is {0}. I am {1} years old.", this.name, this.age);
     }
+
+    @Override
+    public boolean equals(Object o){
+        if (this == o) return true;
+        if (!(o instanceof Person)) return false;
+
+        Person person = (Person) o;
+
+        return id == person.id;
+    }
+
+    @Override
+    public int hashCode(){
+        return id;
+    }
 }
