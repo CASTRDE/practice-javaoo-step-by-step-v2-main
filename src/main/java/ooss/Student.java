2d83c6a85e56e0a413af2474206e0c7d5d8fe2ae
Desiree Castro <desiree.anne.castro@oocl.com>
2022-08-31 17:21:41 +0800

    feat: extended the introduce method from person to the student and teacher class

--- src/main/java/ooss/Student.java
+++ src/main/java/ooss/Student.java
@@ -1,4 +1,14 @@
 package ooss;
 
-public class Student {
+import java.text.MessageFormat;
+
+public class Student extends Person{
+    public Student(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    @Override
+    public String introduce(){
+        return MessageFormat.format("{0} I am a student.", super.introduce());
+    }
 }
--- src/main/java/ooss/Teacher.java
+++ src/main/java/ooss/Teacher.java
@@ -1,4 +1,14 @@
 package ooss;
 
-public class Teacher {
+import java.text.MessageFormat;
+
+public class Teacher extends Person {
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    @Override
+    public String introduce(){
+        return MessageFormat.format("{0} I am a teacher.", super.introduce());
+    }
 }
